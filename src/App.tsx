b485f91a1b71baee0f7eae82c8085b899b870c12
251412 <chaitanyapadalgaduthuri@gamil.com>
2026-09-06 01:54:54 +0530

    remove: mobile action bar (Call Desk / Book Appt / 24/7 ER) from bottom of mobile screens

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import EventPopup from './components/EventPopup';
 import WhatsAppWidget from './components/WhatsAppWidget';
-import MobileActionBar from './components/MobileActionBar';
 
 // Public Pages
 import Home from './pages/Home';
@@ -78,7 +77,6 @@ function LayoutWrapper({ children, adminUser, onLogout }: { children: React.Reac
       <main key={location.pathname} className="page-transition" style={{ minHeight: '80vh', overflow: 'hidden' }}>
         {children}
       </main>
-      {!isAdminDashboard && <MobileActionBar />}
       {!isAdminDashboard && <Footer />}
     </>
   );
